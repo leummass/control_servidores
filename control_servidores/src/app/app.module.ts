6ec7d18dc00abe09ff296e49ea7439bc7827de5a
Samuel <jesuval420@gmail.com>
2023-09-19 00:17:43 -0700

    Avance en login

--- control_servidores/src/app/app.module.ts
+++ control_servidores/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Material
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import{ MatButtonModule} from '@angular/material/button';
+//Componentes
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ control_servidores/src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder){
+    this.form = this.formBuilder.group({
+      empleado: ['', Validators.required]
+    })
+  }
+  login(){
+    console.log(this.form);
+  }
+}
