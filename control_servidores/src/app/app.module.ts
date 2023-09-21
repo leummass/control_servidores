ac1f372b3985616da9761794588620912a4da899
Samuel <jesuval420@gmail.com>
2023-09-21 01:04:03 -0700

    adición de router

--- control_servidores/src/app/app.module.ts
+++ control_servidores/src/app/app.module.ts
@@ -10,11 +10,11 @@ import { LoginComponent } from './components/login/login.component';
 import { LoginService } from './services/login.service';
 import { DatosService } from './services/datos.service';
 import { SharedModule } from './shared-modules/shared/shared.module';
-import { ServiciosComponent } from './components/inicio/servicios/servicios.component';
+
 
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent,ServiciosComponent,ServiciosComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
