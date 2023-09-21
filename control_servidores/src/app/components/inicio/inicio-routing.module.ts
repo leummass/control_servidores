851da1a37330793642cc575ff2c5bf327ee8f4e5
becario.samuel <becario.samuel@coppel.com>
2023-09-20 17:56:15 -0700

    creación de navbar y sidenav

--- control_servidores/src/app/components/inicio/inicio-routing.module.ts
+++ control_servidores/src/app/components/inicio/inicio-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { InicioComponent } from "./inicio.component";
+import { ServiciosAnadirComponent } from "./servicios-anadir/servicios-anadir.component";
+import { ServiciosComponent } from "./servicios/servicios.component";
+import { ServidoresAnadirComponent } from "./servidores-anadir/servidores-anadir.component";
+import { ServidoresComponent } from "./servidores/servidores.component";
 
 const routes: Routes = [
-    {path: '', component: InicioComponent}
+    {path: '', component: InicioComponent, children: [
+        {path: 'servicios', component: ServiciosComponent},
+        {path: 'anadir_servicio', component: ServiciosAnadirComponent},
+        {path: 'servidores', component: ServidoresComponent},
+        {path: 'anadir_servidor', component: ServidoresAnadirComponent}
+    ]}
 ];
 
 @NgModule({
