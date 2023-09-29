127c0c55e90c62f698db00bb075ac0578acb9924
becario.samuel <becario.samuel@coppel.com>
2023-09-28 17:45:38 -0700

    Cambios de estilo en anadir servidores y servicios

--- control_servidores/src/app/components/inicio/servidores-anadir/servidores-anadir.component.ts
+++ control_servidores/src/app/components/inicio/servidores-anadir/servidores-anadir.component.ts
@@ -21,13 +21,10 @@ export class ServidoresAnadirComponent {
   constructor(private formBuilder: FormBuilder){
     this.form = this.formBuilder.group({
       nombre_servidor: ['', Validators.required,],
-      url_prod: ['', Validators.required,],
       ip_prod: ['', [Validators.required,Validators.pattern('(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])')]],
-      url_des: ['', Validators.required,],
       ip_des: ['',  [Validators.required,Validators.pattern('(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])')]],
-      url_test: ['', Validators.required,],
       ip_test: ['',  [Validators.required,Validators.pattern('(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])')]],
-      dns: ['', Validators.required,],
+      ip_drp: ['',  [Validators.required,Validators.pattern('(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])')]],
       descripcion: ['', Validators.required,],
       tipo: ['', Validators.required,]
 
