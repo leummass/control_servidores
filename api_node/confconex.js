3358e4fca1a11d11159ed4d5db9015b49e81c908
becario.samuel <becario.samuel@coppel.com>
2023-10-11 16:56:59 -0700

    cambios node api

--- api_node/confconex.js
+++ api_node/confconex.js
@@ -3,10 +3,7 @@ const conex = {
     password: '1299e3097',
     server: '10.49.123.215',
     database: 'ContabilidadNueva',
-    options:{
-        encrypt: true,
-        enableArithAbort: true
-    }
+    
 };
 
 module.exports = conex;
