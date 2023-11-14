6d01c541fcfb71755e6640b618a5d3408cebfa21
becario.samuel <becario.samuel@coppel.com>
2023-11-13 17:03:20 -0700

    Se añaden filtros por columna

--- control_servidores/src/app/components/inicio/servidores/servidores.component.ts
+++ control_servidores/src/app/components/inicio/servidores/servidores.component.ts
@@ -2,79 +2,89 @@ import { Component, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { PaginatorESP } from '../paginatoresp.component';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 import { ServidoresAnadirComponent } from '../servidores-anadir/servidores-anadir.component';
 import { MatDialog } from '@angular/material/dialog';
 import { DatosService } from 'src/app/services/datos.service';
 import { Catalogo_Servidor } from 'src/app/models/catalogo_servidor.model';
 
-
 @Component({
   selector: 'app-servidores',
   templateUrl: './servidores.component.html',
   styleUrls: ['./servidores.component.css'],
-  providers: [{provide: MatPaginatorIntl, useClass: PaginatorESP}],
+  providers: [{ provide: MatPaginatorIntl, useClass: PaginatorESP }],
 })
 export class ServidoresComponent {
   displayedColumns: string[] = ['IpProduccion', 'Nombre', 'Tipo', 'Estatus'];
   dataSource = new MatTableDataSource<Catalogo_Servidor>();
   form: FormGroup;
-  nombre:string;
-  peso:string;
+  nombre: string;
+  peso: string;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   tipo_serv: any[] = [
-    { value: 'tipo1', viewValue: 'tipo1' },
-    { value: 'tipo2', viewValue: 'tipo2' },
-    { value: 'tipo3', viewValue: 'tipo3' },
+    { value: '', viewValue: 'Todos'},
+    { value: 'Tester', viewValue: 'Tester' },
+    { value: 'Desarrollo', viewValue: 'Desarrollo' },
+    { value: 'Produccion', viewValue: 'Produccion' },
   ];
 
+  constructor(public ventana: MatDialog, private datosService: DatosService,
+    formBuilder: FormBuilder) {
+      this.dataSource.filterPredicate = ((data, filter)=> {
+        const filters = JSON.parse(filter);
+        const a = !filters[1].fIP || data.IpDireccion.includes(filters[1].fIP);
+        const b = !filters[0].fNombre || data.Nombre.toLowerCase().includes(filters[0].fNombre);
+        let c = !filters[2].fTipo;
+        if(filters[2].fTipo != ''){
+          c = !filters[2].fTipo || data.Tipo === filters[2].fTipo;
+        }
+        
+        return a && b && c;
+      } ) as (Catalogo_Servidor:Catalogo_Servidor, string:string) => boolean
 
-  constructor(public ventana: MatDialog, private datosService: DatosService){}
+      this.form = formBuilder.group({
+        fNombre: '',
+        fIP: '',
+        fTipo: '',
+      })
+      this.form.valueChanges.subscribe(value =>{
+        const filter = [{fNombre: value.fNombre}, {fIP: value.fIP}, {fTipo: value.fTipo}];
+        this.dataSource.filter = JSON.stringify(filter);
+      })
+    }
 
-  ngOnInit(){
+  ngOnInit() {
     this.obtenerServidores();
   }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
-  obtenerServidores(){
-    const params = {nombre:'', ipdireccion:'', tipo:''}
-    
-    this.datosService.getServidores(params).subscribe( data => {
-      this.dataSource.data=data;
-    })
+  obtenerServidores() {
+    const params = { nombre: '', ipdireccion: '', tipo: '' };
+
+    this.datosService.getServidores(params).subscribe((data) => {
+      this.dataSource.data = data;
+    });
   }
 
-  filtrarNombre(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-  filtrarIP(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-  filtrarTipo(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
   abrirVentanaAgregar() {
     const dialogRef = this.ventana.open(ServidoresAnadirComponent, {
       width: '70%',
       enterAnimationDuration: '300ms',
       exitAnimationDuration: '300ms',
-      data: {titulo: 'Agregar servidor'},
+      data: { titulo: 'Agregar servidor' },
     });
   }
 
-  abrirVentanaEditar() {
+  abrirVentanaEditar(servidor: Catalogo_Servidor) {
     const dialogRef = this.ventana.open(ServidoresAnadirComponent, {
       width: '70%',
       enterAnimationDuration: '300ms',
       exitAnimationDuration: '300ms',
-      data: {name: 'XD'},
+      data: { titulo: 'Editar servidor', nombre: servidor.Nombre, tipo: servidor.Tipo, descripcion: servidor.Descripcion },
     });
   }
 }
